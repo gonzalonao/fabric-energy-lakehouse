91e8b0e2708c2068bb7bce7d3f8b6449762fa6c8
GONZALO LOPEZ CRESPO <GLOPEZC443@alumnos.imf.com>
2026-07-19 18:06:13 +0000

    chore(gold): add nb_gold_build + nb_gold_mlv shells

---
+++ fabric/gold/nb_gold_build.Notebook/notebook-content.py
@@ -0,0 +1,34 @@
+# Fabric notebook source
+
+# METADATA ********************
+
+# META {
+# META   "kernel_info": {
+# META     "name": "synapse_pyspark"
+# META   },
+# META   "dependencies": {
+# META     "lakehouse": {
+# META       "default_lakehouse": "6cabfc1b-836e-43ad-9379-94fa8bdb6c16",
+# META       "default_lakehouse_name": "lh_energy",
+# META       "default_lakehouse_workspace_id": "476b58fd-19e3-4c0d-bde7-c3f16d2a6fcf",
+# META       "known_lakehouses": [
+# META         {
+# META           "id": "6cabfc1b-836e-43ad-9379-94fa8bdb6c16"
+# META         }
+# META       ]
+# META     }
+# META   }
+# META }
+
+# CELL ********************
+
+# Welcome to your new notebook
+# Type here in the cell editor to add code!
+
+
+# METADATA ********************
+
+# META {
+# META   "language": "python",
+# META   "language_group": "synapse_pyspark"
+# META }
